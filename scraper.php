d6b1ddfe45e0c99516e562d1f6f40adec0f2effd
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-11-22 16:04:54 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,7 +15,7 @@ $BaseLink	=	'http://202.61.43.40:8080/';
 	
 	
 	//	Page pagination
-	for($PageLoop = 1; $PageLoop < $loop; $PageLoop++)
+	for($PageLoop = 1; $PageLoop < 2; $PageLoop++)
 	{
 		
 		$FinalURL  		=  'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page='.$PageLoop;
