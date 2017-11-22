64f17a375a3d2edeeff890ca182ca429bd280c63
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-11-22 16:18:12 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,8 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 $BaseLink	=	'http://202.61.43.40:8080/';
 	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page=1';
-	sleep(5);
 	$Pagination = 	file_get_html($SiteURL);
-echo $Pagination;
+	echo $Pagination;
 	?>
 			
