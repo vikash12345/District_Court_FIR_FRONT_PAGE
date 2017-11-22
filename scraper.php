4c8d9f4b26d844b9fe7c8f6dec0d55da2aa1c0c5
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-11-22 16:04:35 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -31,13 +31,23 @@ $BaseLink	=	'http://202.61.43.40:8080/';
 				if ($RowNumb != 0) 
 				{
 					$no			=	$element->find('./td[1]', 0)->plaintext;
-					$CourtName	=	$element->find('./td[2]', 0)->plaintext;
-					$CaseNumbr	=	$element->find('./td[3]', 0)->plaintext;
-					$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
-					$CaseValue	=	$element->find('./td[5]/button', 0);
-					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
-					$CaseLink	=	str_replace("amp;", "", $CaseLinkR);
+					$courtname	=	$element->find('./td[2]', 0)->plaintext;
+					$casenumbr	=	$element->find('./td[3]', 0)->plaintext;
+					$casestats	=	$element->find('./td[4]', 0)->plaintext;
+					$casevalue	=	$element->find('./td[5]/button', 0);
+					$caselinkR	=	$BaseLink . $CaseValue->attr['value'];
+					$caselink	=	str_replace("amp;", "", $CaseLinkR);
           echo $FinalURL;
+					
+					
+					scraperwiki::save_sqlite(array('num'), array('num' => $no,
+											     				 'courtname' => $courtname,
+															 'casenumbr' => $casenumbr,
+															 'casestats' => $casestats, 
+															 'caselink' => $caselink, 
+														 ));
+					
+					
 				}
 			}
 		}
