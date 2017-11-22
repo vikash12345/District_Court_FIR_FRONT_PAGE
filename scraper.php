cfc28c6369bd5fb08488d1acfafe9df3eeda6a6e
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-11-22 16:21:32 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,6 +4,8 @@ require 'scraperwiki/simple_html_dom.php';
 $BaseLink	=	'http://202.61.43.40:8080/';
 	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page=1';
 	$Pagination = 	file_get_html($SiteURL);
-	echo $Pagination;
+	$numberforloop = $Pagination->find("//*[@id='w0']/div/b[2]",0)->plaintext;
+	$text = str_replace(',', '', $numberforloop);
+	echo $loop = $text/20;
 	?>
 			
